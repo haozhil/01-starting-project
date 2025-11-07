1518f45e9dba1c5bf7860cc8e80046469ef1d891
Hongdouni <haozhil97@gmail.com>
2025-11-07 23:28:24 +0800

    Passing arguments to event functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header';
+import CoreConcepts from './components/CoreConcepts';
+import TabButton from './components/TabButton';
+
 function App() {
+  function selectHandlder(selectedButton) {
+    console.log(selectedButton);
+  }
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcepts
+                {...concept}
+              />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => selectHandlder('Components')}>Components</TabButton>
+            <TabButton onSelect={() => selectHandlder('JSX')}>JSX</TabButton>
+            <TabButton onSelect={() => selectHandlder('Props')}>Props</TabButton>
+            <TabButton onSelect={() => selectHandlder('State')}>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
